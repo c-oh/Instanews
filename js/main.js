a4c7d9a04c4dbbc43b77e7da819e56ee3c7664b1
Sharifa <shar.samuels@gmail.com>
2016-10-18 15:15:37 -0700

    added quotation to links and target_blank so links open in new page

--- js/main.js
+++ js/main.js
@@ -22,7 +22,7 @@ $('.search').change(function () {
     info.splice(12);
     $.each(info, function (key, value) {
         //var url = value.url;
-        var picture = '<a class=links href=' + value.url + "> <div class= articles style= background-image:url(" + value.multimedia[4].url + ")>";
+        var picture = '<a class="links" href="' + value.url + '"target="_blank" rel="noopener"> <div class= "articles" style= background-image:url(" + value.multimedia[4].url + ")>';
         var abstract = "<p>" + value.abstract + "</p></div></a>";
         var info = picture + abstract;
         $('.stories').append(info);
