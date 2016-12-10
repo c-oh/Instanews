52d244b42e34be87cc047213c7670371b8158328
Holly <papiniu@outlook.com>
2016-12-10 13:48:30 -0800

    Fixed eslint and made it responsive to ipad

--- gulpfile.js
+++ gulpfile.js
@@ -1,66 +1,59 @@
 var gulp = require('gulp'),
-  uglify = require('gulp-uglify'),
-  rename = require('gulp-rename'),
-  plumber = require('gulp-plumber'),
-  browserSync = require('browser-sync'),
-  sass = require('gulp-sass'),
-  autoprefixer = require('gulp-autoprefixer'),
-  cssnano = require('gulp-cssnano'),
-  eslint = require('gulp-eslint'),
-  notify = require('gulp-notify'),
-  rename = require('gulp-rename');
+    uglify = require('gulp-uglify'),
+    rename = require('gulp-rename'),
+    browserSync = require('browser-sync').create();
+    gulp = require ('gulp'),
+    babel = require('gulp-babel');
+// Now that we've installed the uglify package we can require it:
+gulp.task('scripts', function(){
+    gulp.src('./js/*.js') // What files do we want gulp to consume?
+      .pipe(uglify()) // Call the uglify function on these files
+      .pipe(rename({ extname: '.min.js' })) //  Rename the uglified file
+      .pipe(gulp.dest('./build/js')) // Where do we put the result?
+});
+gulp.task('watch',function(){
+    gulp.watch('js/*.js',['scripts']);
+    gulp.watch('./sass/*.scss', ['sass']);
+});
+gulp.task('browser-sync',function(){
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+    gulp.watch(['build/css/*.css','build/js/*./js']).on('change', browserSync.reload);
+});
 
-
+const input = 'src/index.js';
+const output = 'dist';
+gulp.task('babel', () => {
+    return gulp.src(input)
+        .pipe(babel())
+        .pipe(gulp.dest(output));
+});
+var sass = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    cssnano = require('gulp-cssnano'),
+    rename = require('gulp-rename'),
+    plumber = require('gulp-plumber'),
+    notify = require('gulp-notify');
 var plumberErrorHandler = {
-  errorHandler: notify.onError({
-    title: 'gulp',
-    message: 'Error: <%= error.message %>' 
-  })
+   errorHandler: notify.onError({
+      title: 'Gulp',
+      message: 'Error: <%= error.message %>'
+   })
 };
 
-gulp.task('js', ['eslint'], function () {
-  gulp.src('./js/*.js') // what files do we want gulp to consume?
-    .pipe(uglify()) //uglify minifies the files, pipe chains files together
-    .pipe(rename({
-      extname: '.min.js'
-    })) //we're renaming the ugly file'
-    .pipe(gulp.dest('./build/js')) //where are we putting the result?
+gulp.task('sass', function() {
+   gulp.src('./sass/style.scss')
+      .pipe(plumber(plumberErrorHandler))
+      .pipe(sass())
+      .pipe(autoprefixer({
+         browsers: ['last 2 versions']
+      }))
+      .pipe(gulp.dest('./'))
+      .pipe(cssnano())
+      .pipe(rename('style.min.css'))
+      .pipe(gulp.dest('./build/css'));
 });
-
-gulp.task('eslint', function () {
-  return gulp.src(['./js/*js'])
-    .pipe(eslint())
-    .pipe(eslint.format())
-    .pipe(eslint.failAfterError());
-});
-
-gulp.task('sass', function () {
-  gulp.src('./sass/style.scss')
-    .pipe(plumber(plumberErrorHandler))
-    .pipe(sass())
-    .pipe(autoprefixer({
-      browsers: ['last 2 versions']
-    }))
-    .pipe(gulp.dest('./build/css'))
-    .pipe(cssnano())
-    .pipe(rename('style.min.css'))
-    .pipe(gulp.dest('./build/css'));
-});
-
-gulp.task('browser-sync', function () {
-  browserSync.init({
-    server: {
-      baseDir: "./",
-    }
-  });
-
-  gulp.watch(["build/css/*.css", "build/js/*.js"]).on('change', browserSync.reload);
-});
-
-//ES LINT
-gulp.task('watch', function () {
-  gulp.watch('js/*.js', ['js', 'eslint']);
-  gulp.watch('sass/*.scss', ['sass']);
-});
-
-gulp.task('default', ['browser-sync', 'watch']);
+gulp.task('default', ['sass', 'scripts','watch','browser-sync','babel']);
--- js/main.js
+++ js/main.js
@@ -2,35 +2,41 @@ $(document).ready(function () {
 
 });
 
-$('.search').change(function () {
+$('#sections').hide({ 
+  'onChange': function () {
     $('.loading_thing').show();
 
-  var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
-  url += '?' + $.param({
-    'api-key':'2cee7ea7b62f4e7d83a741c409dae5e4'
-  });
+    $('header').addClass('header-minify');
 
-  $.ajax({
-    url: url,
-    method: 'GET',
-  })
+ 
+    var optSection = $('.sections').val();
 
-  .done(function (data) {
-    var info = [];
-    info = $(data.results).filter(function (key, value) {
-      return $(value.multimedia).length >= 5;
+    $.ajax({
+      method: 'GET',
+      url: 'http://api.nytimes.com/svc/topstories/v2/' + optSection + '.json?api-key= 2cee7ea7b62f4e7d83a741c409dae5e4',
     })
-    info.splice(12);
-    $.each(info, function (key, value) {
-        //var url = value.url;
-        var picture = '<a class=links href=' + value.url +'> <div class= articles style= background-image:url(' + value.multimedia[4].url +' )>';
-        var abstract = '<p>' + value.abstract + '</p></div></a>';
-        var info = picture + abstract;
-        $('.stories').append(info);
-      }) 
-      
-  })
-  .always(function(){
-          $('.loading_thing').hide();
-    }); 
-});
+
+
+
+    var $news = $('.news')
+      .done(function (data) {
+        $news.empty();
+        var info = [];
+        info = $(data.results).filter(function (key, value) {
+          return $(value.multimedia).length >= 5;
+        })
+        info.splice(12);
+        $.each(info, function (key, value) {
+          //var url = value.url;
+          var picture = '<a class=links href=' + value.url + '> <div class= articles style= background-image:url(' + value.multimedia[4].url + ' )>';
+          var abstract = '<p>' + value.abstract + '</p></div></a>';
+          var info = picture + abstract;
+          $('.stories').append(info);
+        })
+
+      })
+      .always(function () {
+        $('.loading_thing').hide();
+      })
+  }
+})
