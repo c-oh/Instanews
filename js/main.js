44b8181e14c13292dfad72b9f89bf2ce878db3b1
Holly <papiniu@outlook.com>
2016-12-10 14:00:10 -0800

    Updated selection list

--- js/main.js
+++ js/main.js
@@ -2,41 +2,35 @@ $(document).ready(function () {
 
 });
 
-$('#sections').hide({ 
-  'onChange': function () {
+$('#sections').change(function () {
     $('.loading_thing').show();
 
-    $('header').addClass('header-minify');
+  var url = 'https://api.nytimes.com/svc/topstories/v2/home.json';
+  url += '?' + $.param({
+    'api-key':'2cee7ea7b62f4e7d83a741c409dae5e4'
+  });
 
- 
-    var optSection = $('.sections').val();
+  $.ajax({
+    url: url,
+    method: 'GET',
+  })
 
-    $.ajax({
-      method: 'GET',
-      url: 'http://api.nytimes.com/svc/topstories/v2/' + optSection + '.json?api-key= 2cee7ea7b62f4e7d83a741c409dae5e4',
+  .done(function (data) {
+    var info = [];
+    info = $(data.results).filter(function (key, value) {
+      return $(value.multimedia).length >= 5;
     })
-
-
-
-    var $news = $('.news')
-      .done(function (data) {
-        $news.empty();
-        var info = [];
-        info = $(data.results).filter(function (key, value) {
-          return $(value.multimedia).length >= 5;
-        })
-        info.splice(12);
-        $.each(info, function (key, value) {
-          //var url = value.url;
-          var picture = '<a class=links href=' + value.url + '> <div class= articles style= background-image:url(' + value.multimedia[4].url + ' )>';
-          var abstract = '<p>' + value.abstract + '</p></div></a>';
-          var info = picture + abstract;
-          $('.stories').append(info);
-        })
-
-      })
-      .always(function () {
-        $('.loading_thing').hide();
-      })
-  }
-})
+    info.splice(12);
+    $.each(info, function (key, value) {
+        //var url = value.url;
+        var picture = '<a class=links href=' + value.url +'> <div class= articles style= background-image:url(' + value.multimedia[4].url +' )>';
+        var abstract = '<p>' + value.abstract + '</p></div></a>';
+        var info = picture + abstract;
+        $('.stories').append(info);
+      }) 
+      
+  })
+  .always(function(){
+          $('.loading_thing').hide();
+    }); 
+});
