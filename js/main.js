7c53e1a21a96d85c5cc586d25d6a078c05b13350
Holly <papiniu@outlook.com>
2016-12-10 16:48:08 -0800

    Fixed footer

--- js/main.js
+++ js/main.js
@@ -9,9 +9,9 @@ $('#sections').change(function () {
 
   var $news = $('.stories');
 
-  var optSelect = $('#sections').val();
+  var userSelect = $('#sections').val();
 
-  var url = 'https://api.nytimes.com/svc/topstories/v2/' + optSelect + '.json';
+  var url = 'https://api.nytimes.com/svc/topstories/v2/' + userSelect + '.json';
 
   url += '?' + $.param({
     'api-key':'2cee7ea7b62f4e7d83a741c409dae5e4'
@@ -28,17 +28,24 @@ $('#sections').change(function () {
     info = $(data.results).filter(function (key, value) {
       return $(value.multimedia).length >= 5;
     })
+
     info.splice(12);
+
     $.each(info, function (key, value) {
-        //var url = value.url;
-        var picture = '<a class=links href=' + value.url +'> <div class= articles style= background-image:url(' + value.multimedia[4].url +' )>';
+        var picture = '<a class=links href=' + value.url +'> <div class= "articles" style= background-image:url(' + value.multimedia[4].url +' )>';
         var abstract = '<p>' + value.abstract + '</p></div></a>';
         var info = picture + abstract;
         $('.stories').append(info);
       }) 
       
   })
+
   .always(function(){
           $('.loading_thing').hide();
-    }); 
+    })
+
+  .fail(function(error){
+      $news.append('<p class="error"> Sorry! There is a problem with your request, please try again. </p>');
+    })
+
 });
