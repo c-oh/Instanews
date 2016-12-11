032a8f547209664f63a70c31678f4044f1c549b8
Holly <papiniu@outlook.com>
2016-12-10 19:21:17 -0800

    Added comments

--- js/main.js
+++ js/main.js
@@ -6,14 +6,17 @@ $('#mySelect').heapbox({
   'onChange': function(){
     $('.loading_thing').show();
     
-
+//When the user selects the category, the site size and header will shrink
  $('.headers').addClass('header-minify');
   $('.site').addClass('site-minify');
 
+//This is the API stories area
   var $news = $('.stories');
 
+//The selection of the user
   var userSelect = $('#mySelect').val();
 
+//Calling API with the selection of the user
   var url = 'https://api.nytimes.com/svc/topstories/v2/' + userSelect + '.json';
 
   url += '?' + $.param({
@@ -26,7 +29,9 @@ $('#mySelect').heapbox({
   })
 
   .done(function (data) {
+    //The article stories area becomes empty
     $news.empty();
+    //The info variable represents output of API
     var info = [];
     info = $(data.results).filter(function (key, value) {
       return $(value.multimedia).length >= 5;
